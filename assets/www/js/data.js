cb8647d4146f984505e3766180642adb326503aa
Micah Lee <micahflee@riseup.net>
2012-07-13 07:44:08 -0700

    hack to fix timestamp formats

--- assets/www/js/data.js
+++ assets/www/js/data.js
@@ -2,6 +2,10 @@ var data = {
     d : null, 
     load : function() {
         data.d = JSON.parse(window.JSInterface.getScheduleJson(false));
+        // fix timestamps
+        for(var i in data.d) {
+          data.d[i].timestamp -= (60*60*4);
+        }
     },
     loadForce : function() {
         data.d = JSON.parse(window.JSInterface.getScheduleJson(true));
